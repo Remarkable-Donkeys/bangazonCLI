76b1087ee02701444285d47173739603461a38f1
Sean Williams <dacamor85@gmail.com>
2018-02-09 15:46:31 -0600

    Popular items and Revenue Report WIP

--- src/Managers/RevenueReportManager.cs
+++ src/Managers/RevenueReportManager.cs
@@ -6,6 +6,40 @@ namespace bangazonCLI
 {
     public class RevenueReportManager
     {
-        
+        private DatabaseInterface db;
+
+        public RevenueReportManager(string dbInterface)
+        {
+            db = new DatabaseInterface(dbInterface);
+        } 
+        public RevenueReportManager()
+        {
+            db = new DatabaseInterface("BANGAZONCLI");
+        }
+
+        public Dictionary<string,double> GetProductsDictionary(Order input)
+        {
+            Dictionary<string, double> resDictionary = new Dictionary<string,double>();
+            List<Product> productList = input.GetProductList();
+            double total = 0;
+
+            foreach (Product item in productList)
+            {
+                if (resDictionary.ContainsKey(item.Id.ToString()))
+                {
+                    resDictionary[item.Id.ToString()] += item.Price;
+                    total += item.Price;
+                } 
+                else
+                {
+                    resDictionary.Add(item.Id.ToString(), item.Price);  
+                    total += item.Price;
+                }
+            }
+
+            resDictionary.Add("Total", total);
+
+            return resDictionary;
+        }
     }
 }
--- test/RevenueReportManagerShould.cs
+++ test/RevenueReportManagerShould.cs
@@ -16,18 +16,107 @@ namespace bangazonCLI.Test
         private RevenueReportManager _manager;
         private Order _order;
         private DatabaseInterface db;
+		private Customer _testCustomer;
 
-        // public RevenueReportManagerShould()
-        // {
-		// 	db = new DatabaseInterface("BANGAZONTEST");
-        //     _manager = new RevenueReportManager("BANGAZONTEST");
-        //     _orders = OrderManager.GetSingleOrder(1);
-        // }
+        private OrderManager _orderManager;
+
+		private CustomerManager _customerManager;
+        private ProductManager _productManager;
+
+        public RevenueReportManagerShould()
+        {
+			db = new DatabaseInterface("BANGAZONTEST");
+            _manager = new RevenueReportManager("BANGAZONTEST");
+			_testCustomer = new Customer("Sean", "Williams");
+			_customerManager = new CustomerManager("BANGAZONTEST");
+			_customerManager = new OrderManager("BANGAZONTEST");
+            _productManager = new ProductManager("BANGAZONTEST");
+             _order = new Order();
+        }
 
         [Fact]
 
-        public void DisplayOrder()
+        public void GetProductsDictionary()
         {
+            db.NukeDB();
+            db.CheckDatabase();
+            _order.DateCreated = DateTime.Now;
+            _order.CustomerId = _customerManager.Add(_testCustomer);
+            Product _product = new Product("Book", "A BOOK", 25.55, 2);
+            Product _product2 = new Product("Book", "A BOOK", 50.5, 2);
+            _product.CustomerId = _order.CustomerId;
+            _product.Id =  _productManager.Add(_product);
+            _product2.Id =  _product2.CustomerId = _order.CustomerId;
+            _productManager.Add(_product2);
+            _order.AddProduct(_product);
+            _order.AddProduct(_product);
+            _order.AddProduct(_product);
+            _order.AddProduct(_product);
+            _order.AddProduct(_product2);
+
+            Dictionary<string,double> productDictionay = _manager.GetProductsDictionary(_order);
+            double res = 0.0;
+
+            foreach (KeyValuePair<string, double> item in productDictionay)
+            {
+                res += item.Value;
+            }
+            // res/2 because res = the price of all products + the total price
+            // so res/2 = total price
+            Assert.Equal(152.7, res/2);
+            Assert.Equal(152.7, productDictionay["Total"]);
+            Assert.Equal(102.2, productDictionay[_product.Id.ToString()]);
+        }
+
+        [Fact]
+
+        public void GetPopularItems()
+        {
+            db.NukeDB();
+            db.CheckDatabase();
+
+            _order.DateCreated = DateTime.Now;
+            _order.CustomerId = _customerManager.Add(_testCustomer);
+            Product _product = new Product("Book", "A BOOK", 25.55, 2);
+            Product _product2 = new Product("Book2", "A BOOK", 50.5, 2);
+            Product _product3 = new Product("Book3", "A BOOK", 50.5, 2);
+            Product _product4 = new Product("Book4", "A BOOK", 50.5, 2);
+
+            _product.CustomerId = _order.CustomerId;
+            _product.Id =  _productManager.Add(_product);
+
+            _product2.Id =  _product2.CustomerId = _order.CustomerId;
+            _product2.Id =  _productManager.Add(_product2);
+
+            _product3.Id =  _product3.CustomerId = _order.CustomerId;
+            _product3.Id =  _productManager.Add(_product3);
+
+            _product4.Id =  _product4.CustomerId = _order.CustomerId;
+            _product4.Id =  _productManager.Add(_product4);
+            
+            _order.AddProduct(_product);
+            _order.AddProduct(_product);
+            _order.AddProduct(_product);
+            _order.AddProduct(_product);
+            _order.AddProduct(_product2);
+            _order.AddProduct(_product2);
+            _order.AddProduct(_product3);
+            _order.AddProduct(_product3);
+            _order.AddProduct(_product3);
+            _order.AddProduct(_product4);
+
+            _orderManager.AddOrder(_order);
+            _orderManager.AddOrder(_order);
+            _orderManager.AddOrder(_order);
+            _orderManager.AddOrder(_order);
+            _orderManager.AddOrder(_order);
+            _orderManager.AddOrder(_order);
+
+            List<Order> orderList = _orderManager.GetOrderList();
+
+            Dictionary<string, double> popularItems = _manager.GetPopularItems(orderList);
+
+            Assert.Equal()
 
         }
     }
